f7d25f78d7644ab4fcdb4b315db6c0f5f28e13c4
Priscila Araujo <araujo16012002@gmail.com>
2019-07-19 17:07:01 -0300

    Adicionar atividade n°2

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let idade = Number(prompt("Qual a sua idade?"));
+/*let idade = Number(prompt("Qual a sua idade?"));
     
 if (idade >= 60) {
     console.log ("Entrada Franca");
@@ -18,4 +18,20 @@ else if(idade >=18 && idade <=59) {
             console.log ("Paga 17,50 reais a entrada")
         else
             console.log ("Paga 35,00 reais a entrada");
+}*/
+let idade = Number(prompt("Digite a sua idade"))
+    if (idade >=25 && idade <=60){
+        let renda = Number(prompt("Digite a sua renda mensal"))
+            if (renda >=2000){
+                let valorDoEmprestimo = Number(prompt("Digite o valor do empréstimo"))
+                if (valorDoEmprestimo <10*renda){
+                    console.log ("Empréstimo aprovado")
+                }else{
+                    console.log ("Empréstimo recusado pelo valor pedido")
+                }
+            }else{
+                console.log ("Empréstimo recusado pela renda")
+            }
+    }else{
+        console.log("Empréstimo recusado pela idade")
 }
