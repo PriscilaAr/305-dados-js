dd7fa751badc30c5ecdef09c46d7ad885d078348
Priscila Araujo <araujo16012002@gmail.com>
2019-07-19 17:18:19 -0300

    Adicionar atividade n°1

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ else if(idade >=18 && idade <=59) {
         else
             console.log ("Paga 35,00 reais a entrada");
 }*/
-let idade = Number(prompt("Digite a sua idade"))
+/*let idade = Number(prompt("Digite a sua idade"))
     if (idade >=25 && idade <=60){
         let renda = Number(prompt("Digite a sua renda mensal"))
             if (renda >=2000){
@@ -34,4 +34,27 @@ let idade = Number(prompt("Digite a sua idade"))
             }
     }else{
         console.log("Empréstimo recusado pela idade")
+}*/
+let dia = Number (prompt("Que dia da semana é hoje?"));
+
+if (dia == 1){
+    console.log ("Hoje é domingo");
+}
+else if (dia == 2){
+    console.log ("Hoje é segunda-feira");
+}
+else if (dia == 3){
+    console.log ("Hoje é terça-feira");
+}
+else if (dia == 4){
+    console.log ("Hoje é quarta-feira");
+}
+else if (dia == 5){
+    console.log ("Hoje é quinta-feira");
+}
+else if (dia == 6){
+    console.log ("Hoje é sexta-feira");
+}
+else if (dia == 7){
+    console.log ("Hoje é sábado");
 }
